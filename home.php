475523453fe3265d306563b727eb1bd36c9c30d3
Frenol021 <frenol021@gmail.com>
2022-04-06 07:40:45 +0300

    first commit

---
+++ home.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+<!-- swiper link -->
+    <link rel="stylesheet"href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="style.css">
+    
+
+</head>
+<body>
+    
+    <section class="header">
+        <b><a href= "home.php" class="logo">travel.</a></b>
+
+        <nav class="navbar">
+            <a href="home.php"><b>home</b></a>
+            
+            <a href="book.php"><b> book </b></a>
+            <a href="package.php"><b> package </b></a>
+            
+            <a href="about.php">about</a>
+
+        </nav>
+
+        <div id= "menu-btn" class="fas fa-bars"></div>
+
+
+    </section>
+
+
+<!-- services section -->
+
+<div class="services">
+    <h1 class="heading-title">our services</h1>
+    <div class="box-container">
+
+        <div class="box">
+        <img src="images/travel2.jpg" alt="">
+        <h3>adventure</h3>
+        </div>
+
+        <div class="box">
+        <img src="images/travel10.jpeg" alt="">
+        <h3>camp fire</h3>
+        </div>
+
+        <div class="box">
+        <img src="images/travel4.jpeg" alt="">
+        <h3>vacation</h3>
+        </div>
+
+        <div class="box">
+        <img src="images/travel8.jpg" alt="">
+        <h3>adventure</h3>
+        </div>
+
+
+
+    </div>
+</div>
+
+
+
+
+<!-- footer-->
+<section class="footer">
+   <div class="box-container">
+       <div class="box">
+           <h3> qick links</h3>
+       <a href="home.php"><i class=" fas fa-angle-right"></i> home</a>
+            <a href="about.php"><i class=" fas fa-angle-right"></i>about</a>
+            <a href="package.php"><i class=" fas fa-angle-right"></i>book</a>
+            <a href="book.php"><i class=" fas fa-angle-right"></i>package</a>
+
+       </div>
+
+       <div class="box">
+           <h3> extra links</h3>
+       <a href="#"><i class=" fas fa-angle-right"></i> ask questions</a>
+            <a href="#"><i class=" fas fa-angle-right"></i>about us</a>
+            <a href="#"><i class=" fas fa-angle-right"></i>privacy policy</a>
+            <a href="#"><i class=" fas fa-angle-right"></i>terms of use</a>
+
+       </div>
+   
+
+   <div class="box">
+           <h3> contacts</h3>
+       <a href="#"><i class=" fas fa-phone"></i> 0790 698 026</a>
+       <a href="#"><i class=" fas fa-phone"></i> 0706 329 253</a>
+       <a href="#"><i class=" fas fa-email"></i> frenol021@gmail.com</a> 
+       <a href="#"><i class=" fas fa-map"></i> waiyaki way, Nairobi</a>     
+
+       </div>
+
+
+   <div class="box">
+           <h3>follow us</h3>
+       <a href="#"><i class=" fab fa-facebook"></i> facebook</a>
+       <a href="#"><i class=" fab fa-twitter"></i> twitter</a>
+       <a href="#"><i class=" fab fa-instagram"></i> instagram</a>
+       <a href="#"><i class=" fab fa-linkedin"></i> linkedin</a>    
+
+       </div>
+    </div>
+
+    <div class="credit"><b>created by <span>frenol</b></span></div>
+       
+</section>
+
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+
+<script src="script.js"></script>
+</body>
